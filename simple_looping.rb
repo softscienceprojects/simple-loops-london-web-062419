7de10004b0bd0e3ee272ef17a01d188acd2105cb
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-11 13:16:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,6 +8,7 @@ def loop_iterator(number_of_times)
     counter += 1
     if counter == number_of_times
       break
+    end
   end
 end
 
@@ -17,7 +18,6 @@ def times_iterator(number_of_times)
   number_of_times.times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
-
 end
 
 def while_iterator(number_of_times)
